e7151e7a88f94199947fd45731d094b500d49b34
Davis Landry <davislandry@Daviss-MacBook-Pro.local>
2018-04-14 16:50:21 -0600

    new main and implementation of maze.c

--- maze.c
+++ maze.c
@@ -16,3 +16,59 @@ void printArr(int arr[8][8],int row, int col){
     }
   }
 }
+
+
+int loadMaze(){
+  /*
+  FILE * mazeFile = NULL;
+  char buff[100];
+  char fileName[30];
+  
+  memset(buff,0,sizeof(buff));
+  
+  //asks for user to imput file
+  printf("Please Enter Maze File then Press Enter\n");
+  scanf("%s",fileName);
+
+  mazeFile = fopen(fileName,"r");
+  if(!mazeFile){
+    printf("File was not correctly oppened");
+    return -1;
+  }
+
+  */
+
+
+  FILE * maze;
+  int const ROW = 8, COL = 8;
+  int mz[ROW][COL];
+  char c;
+  int row = 0, col = 0;
+  char fileName[30];
+
+  //asks for user to imput file
+  printf("Please Enter Maze File then Press Enter\n");
+  scanf("%s",fileName);
+
+  //opens the given file and reassignes it to a 2 Dimmentional array
+  maze = fopen(fileName,"r");
+  assert(maze);
+  if(maze){
+    while((c = getc(maze)) != EOF){
+      if(c == '\n')
+	continue;
+      if(c == '.')
+	mz[row][col] = 0;
+      else
+	mz[row][col] = 1;
+      col++;
+      if(col == 8){
+	row++;
+	col = 0;
+      }
+    }
+  }
+  fclose(maze);
+  return mz;
+
+}
