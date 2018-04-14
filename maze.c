cb9104147196712fa151f79452ec5cd840a555a2
Davis Landry <davislandry1@gmail.com>
2018-04-14 13:28:15 -0600

    maze.c is the current driver of the program

---
+++ maze.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+
+void printArr(char arr[8][8],int row, int col){
+  for(int i = 0; i < row; i ++)
+    for(int j = 0; j < col; j++)
+      printf("%c",arr[i][j]);
+}
+
+
+int main(){
+  //import the mazep just to start
+  FILE * maze;
+  int const ROW = 8, COL = 8;
+  char mz[ROW][COL];
+  char c;
+  int row = 0, col = 0;
+  
+  maze = fopen("maze.txt","r");
+  if(maze){
+    while((c = getc(maze)) != EOF){
+      mz[row][col] = c;
+      col++;
+      if(col == 8){
+	row++;
+	col = 0;
+      }
+    }
+  }
+  printArr(mz,ROW,COL);
+}
