113852fd9f91cbaa359d861f9918dcb09e713b9d
Davis Landry <davislandry@Daviss-MacBook-Pro.local>
2018-04-14 16:07:12 -0600

    changes it to 1 and 0

--- maze.c
+++ maze.c
@@ -1,12 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
 
 
-void printArr(char arr[8][8],int row, int col){
+/*
+The is a function that prints out the array that has been made
+ */
+void printArr(int arr[8][8],int row, int col){
   for(int i = 0; i < row; i ++){
     for(int j = 0; j < col; j++){
-      printf("%c",arr[i][j]);
+      printf("%d",arr[i][j]);
       if(j+1 == col)
 	printf("\n");
     }
@@ -15,23 +19,29 @@ void printArr(char arr[8][8],int row, int col){
 
 
 int main(){
-  //import the mazep just to start
+
   FILE * maze;
   int const ROW = 8, COL = 8;
-  char mz[ROW][COL];
+  int mz[ROW][COL];
   char c;
   int row = 0, col = 0;
   char fileName[30];
 
+  //asks for user to imput file
   printf("Please Enter Maze File then Press Enter\n");
   scanf("%s",fileName);
-  
+
+  //opens the given file and reassignes it to a 2 Dimmentional array
   maze = fopen(fileName,"r");
+  assert(maze);
   if(maze){
     while((c = getc(maze)) != EOF){
       if(c == '\n')
 	continue;
-      mz[row][col] = c;
+      if(c == '.')
+	mz[row][col] = 0;
+      else
+	mz[row][col] = 1;
       col++;
       if(col == 8){
 	row++;
@@ -40,4 +50,6 @@ int main(){
     }
   }
   printArr(mz,ROW,COL);
+  fclose(maze);
+
 }
