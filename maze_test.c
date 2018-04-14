145e9b3c13ac0c747167894bdf5be6491ed1d94f
Davis Landry <davislandry1@gmail.com>
2018-04-14 16:28:54 -0600

    updated file

--- maze_test.c
+++ maze_test.c
@@ -2,20 +2,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
-
-
-/*
-The is a function that prints out the array that has been made
- */
-void printArr(int arr[8][8],int row, int col){
-  for(int i = 0; i < row; i ++){
-    for(int j = 0; j < col; j++){
-      printf("%d",arr[i][j]);
-      if(j+1 == col)
-	printf("\n");
-    }
-  }
-}
+#include "maze.h"
 
 
 int main(){
