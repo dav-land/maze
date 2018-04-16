3aa8d1a42dd254228992998976446ab2e4ebf1db
Davis Landry <davislandry1@gmail.com>
2018-04-16 16:11:09 -0600

    ordered things in maze.h same as maze.c

--- maze.h
+++ maze.h
@@ -4,14 +4,14 @@
 /* Prints a 2D array */
 void printArr(int row, int col,int arr[][col]);
 
+/* find elements being used in order to get side length of matrix */
+int findNumMazeElements(char * array, int length); 
+
 /* Fills 1D array full of all of the different characters in text file*/
 void loadCharArrFromFile(char * buff);
 
 /* fill array with '~', a blank character*/
 void arrayFill(char * arr);
-
-/* find elements being used in order to get side length of matrix */
-int findNumMazeElements(char * array, int length); 
 
 /* create a 2D maze array */
 void createMazeMatrix(char * arr, int length, int sideLength, int mz[][sideLength]);
